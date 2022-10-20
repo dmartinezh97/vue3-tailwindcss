c065866336413f53341042f9b18eb14d2f145914
dmartinezh97 <dmartinezh97@gmail.com>
2022-10-20 20:41:51 +0200

    Modificar información general

--- src/views/negocios/informacion/InformacionGeneralView.vue
+++ src/views/negocios/informacion/InformacionGeneralView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref, computed } from 'vue';
+import { onMounted, ref, computed, watchEffect, watch, readonly } from 'vue';
 import { onBeforeRouteUpdate, useRoute } from 'vue-router';
 import { useNegocioStore } from '@/stores/modulos/negocio';
 import type { NegocioInformacionGeneralModel } from '@/api/model/negocioModel';
@@ -17,15 +17,34 @@ import { convertToSlug } from '../../../utils/slug';
 const negocioStore = useNegocioStore();
 const { params } = useRoute()
 
-let imgLogo = ref(null)
-let imgCabecera = ref(null)
-let info = ref<NegocioInformacionGeneralModel | null>();
-let infoInicial = ref<NegocioInformacionGeneralModel | null>();
+let info = ref<NegocioInformacionGeneralModel>();
+/* Info */
+let nombre = ref("")
+let slug = ref("")
+let descripcion = ref("")
+let ubicacion = ref("")
+/* Varias variables de imagen por problemas de tipos */
+let imgLogo = ref("")
+let imgLogoFile = ref(null)
+let imgCabecera = ref("")
+let imgCabeceraFile = ref(null)
+/* Info */
 let showModalUbicacion = ref(false)
 let showModalGuardarCambios = ref(false)
+let btnDisabled = ref(true)
 
 onMounted(async () => {
   await getInfo(params.id.toString())
+  /* Para obtener varios valores watch([nombre], ([newValue1], [oldValue1]) => { */
+  watch([nombre, slug, descripcion, ubicacion], ([newNombre, newSlug, newDescripcion, newUbicacion]) => {
+    if(info.value){
+      info.value.nombre = newNombre
+      info.value.slug = newSlug
+      info.value.descripcion = newDescripcion
+      info.value.ubicacion = newUbicacion
+    }
+    btnDisabled.value = false
+  })
 });
 
 onBeforeRouteUpdate(async (onRouteChange)=> {
@@ -36,25 +55,29 @@ onBeforeRouteUpdate(async (onRouteChange)=> {
 const getInfo = async (id: string) => {
   if(id){
     const res = await negocioStore.getInformacionGeneral(id);
-    info.value = { ...res };
-    infoInicial.value = { ...res };
-    info.value.ubicacion = "ubicacion"
+    if(res){
+      info.value = res;
+      nombre.value = res.nombre
+      slug.value = res.slug
+      descripcion.value = res.descripcion
+      ubicacion.value = res.ubicacion
+      imgLogo.value = res.img_logo
+      imgCabecera.value = res.img_cabecera
+      btnDisabled.value = true
+    }
   }
 };
 
 const onChangeNombre = () => {
-  //info.value.nombre = e.target.value;
-  if(info.value){
-    info.value.slug = convertToSlug(info.value.nombre)
-  }
+  slug.value = convertToSlug(nombre.value)
 };
 
 const onChangeFileImgPerfil = (e: any) => {
   if (e.target.files.length > 0) {
-    imgLogo.value = e.target.files[0];
-    if (imgLogo.value != null){
+    imgLogoFile.value = e.target.files[0];
+    if (imgLogoFile.value != null){
       const formData = new FormData();
-      formData.append('logo', imgLogo.value)
+      formData.append('logo', imgLogoFile.value)
       negocioStore.updateLogo(parseInt(params.id.toString()), formData)
     }
   }
@@ -62,19 +85,17 @@ const onChangeFileImgPerfil = (e: any) => {
 
 const onChangeFileImgCabecera = (e: any) => {
   if (e.target.files.length > 0) {
-    imgCabecera.value = e.target.files[0];
-    if (imgCabecera.value != null){
+    imgCabeceraFile.value = e.target.files[0];
+    if (imgCabeceraFile.value != null){
       const formData = new FormData();
-      formData.append('img_cabecera', imgCabecera.value)
+      formData.append('img_cabecera', imgCabeceraFile.value)
       negocioStore.updateCabecera(parseInt(params.id.toString()), formData)
     }
   }
 };
 
 const onChangeUbicacion = (url: string) => {
-  if(info.value){
-    info.value.ubicacion = url
-  }
+  ubicacion.value = url
 };
 
 const onClickBtnConfirmarGuardarCambios = () => {
@@ -97,34 +118,30 @@ const onClickGuardarCambios = async () => {
 }
 
 const getImageLogo = computed(() => {
-  if (imgLogo.value) {
+  if (imgLogoFile.value) {
     var urlCreator = window.URL || window.webkitURL;
-    return urlCreator.createObjectURL(imgLogo.value);
-  } else if(info.value && info.value.img_logo) {
-    return info.value.img_logo
+    return urlCreator.createObjectURL(imgLogoFile.value);
+  } else if(imgLogo.value) {
+    return imgLogo.value
   } else return "";
 })
 
 const getImageCabecera = computed(() => {
-  if (imgCabecera.value) {
+  if (imgCabeceraFile.value) {
     var urlCreator = window.URL || window.webkitURL;
-    return urlCreator.createObjectURL(imgCabecera.value);
-  } else if(info.value && info.value.img_cabecera) {
-    return info.value.img_cabecera
+    return urlCreator.createObjectURL(imgCabeceraFile.value);
+  }  else if(imgCabecera.value) {
+    return imgCabecera.value
   } else return "";
-})
-
-const unchanged = computed(() => {
-  return JSON.stringify(info.value) === JSON.stringify(infoInicial.value)
 })
 
 </script>
 
 <template>
   <div id="informaciongeneral">
-    <div v-if="info" class="max-w-7xl mx-auto overflow-hidden bg-white rounded-lg shadow-md">
+    <div class="max-w-7xl mx-auto overflow-hidden bg-white rounded-lg shadow-md">
         <div class="relative">
-          <img v-if="imgCabecera || info.img_cabecera" class="object-cover w-full h-64" :src="getImageCabecera" alt="Foto cabecera">
+          <img v-if="imgCabecera" class="object-cover w-full h-64" :src="getImageCabecera" alt="Foto cabecera">
           <div v-else class="object-cover w-full h-64 bg-gray-200" alt="Foto cabecera"></div>
           <label for="foto-cabecera" class="absolute z-40 top-3 md:top-auto md:bottom-3 right-3 p-2 bg-primary text-gray-600 md:bg-uno-500 md:text-white border md:border-uno-500 md:hover:bg-dos rounded-lg cursor-pointer font-medium md:px-3 md:py-2 transition ease-in duration-150">
             <div class="hidden md:flex md:items-center">
@@ -141,7 +158,7 @@ const unchanged = computed(() => {
           <label for="foto-logo" class="relative cursor-pointer">
             <div class="relative rounded-full overflow-hidden border-4 border-uno-500">
                 <div class="absolute z-20 w-full h-full hover:bg-black/40"></div>
-                <img v-if="imgLogo || info.img_logo" class="object-cover w-48 h-48" alt="Foto perfil" :src="getImageLogo">
+                <img v-if="imgLogo" class="object-cover w-48 h-48" alt="Foto perfil" :src="getImageLogo">
                 <div v-else class="object-cover w-48 h-48 bg-gray-200" alt="Foto perfil"></div>
               </div>
               <div class="absolute z-30 bottom-3 right-3 rounded-full bg-primary p-2">
@@ -152,10 +169,10 @@ const unchanged = computed(() => {
         </div>
         <div class="p-6 pt-3">
           <div>
-              <InputText label="Nombre" v-model="info.nombre" @keyup="onChangeNombre" class="mb-4"></InputText>
-              <InputText label="Slug" v-model="info.slug" class="mb-4"></InputText>
-              <InputTextarea label="Descripción" v-model="info.descripcion" class="mb-4"></InputTextarea>
-              <InputText label="Ubicación" v-model="info.ubicacion" @click="showModalUbicacion=!showModalUbicacion" class="mb-4" readonly></InputText>
+              <InputText label="Nombre" v-model="nombre" @keyup="onChangeNombre" class="mb-4"></InputText>
+              <InputText label="Slug" v-model="slug" class="mb-4"></InputText>
+              <InputTextarea label="Descripción" v-model="descripcion" class="mb-4"></InputTextarea>
+              <InputText label="Ubicación" v-model="ubicacion" @click="showModalUbicacion=!showModalUbicacion" class="mb-4" readonly></InputText>
               <!-- <iframe v-if="!esEditable && info.ubicacion" :src="info.ubicacion" width="100%" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
               <div v-else-if="!esEditable && !info.ubicacion" class="flex mb-4">
                 <IconDanger class="text-yellow-500"></IconDanger>
@@ -163,7 +180,7 @@ const unchanged = computed(() => {
               </div> -->
           </div>
           <div class="flex justify-end">
-            <BasicButton text="Guardar cambios" @click="onClickBtnConfirmarGuardarCambios" :disabled="unchanged">
+            <BasicButton text="Guardar cambios" @click="onClickBtnConfirmarGuardarCambios" :disabled="btnDisabled">
               <template v-slot:icon>
                 <IconSave class="w-5 h-5 mr-2"></IconSave>
               </template>
@@ -172,7 +189,7 @@ const unchanged = computed(() => {
         </div>
     </div>
     <!-- DIALOGS -->
-    <DialogGoogleMaps v-if="info && showModalUbicacion" v-model="showModalUbicacion" :direccion="info.ubicacion" @submit="onChangeUbicacion"></DialogGoogleMaps>
+    <DialogGoogleMaps v-if="ubicacion && showModalUbicacion" v-model="showModalUbicacion" :direccion="ubicacion" @submit="onChangeUbicacion"></DialogGoogleMaps>
     <DialogOkCancel v-model="showModalGuardarCambios" @submit="onClickGuardarCambios" titulo="¿Desea guardar los cambios?" descripcion="Si quieres guardar algún cambio que hayas hecho, tan solo debes hacer clic sobre el botón Aceptar."></DialogOkCancel>
     <!-- DIALOGS -->
   </div>
