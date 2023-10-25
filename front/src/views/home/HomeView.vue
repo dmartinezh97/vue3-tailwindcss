ed5e2802b0c26ca16a40c3e4edbdfe31c4801992
Daniel martínez <dmartinezh97@gmail.com>
2023-10-25 14:43:08 +0200

    Change text

--- front/src/views/home/HomeView.vue
+++ front/src/views/home/HomeView.vue
@@ -98,7 +98,7 @@ onMounted(() => {
               <div class="flex w-11/12 flex-row-reverse">
                 <div class="relative max-w-xl rounded-xl rounded-tr-none bg-blue-600 px-4 py-2">
                   <span class="text-sm font-medium text-white">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 1
+                    Aquí va la pregunta que acabamos de hacer
                   </span>
                 </div>
               </div>
@@ -107,7 +107,7 @@ onMounted(() => {
               <div class="flex w-11/12">
                 <div class="relative max-w-xl rounded-xl rounded-tl-none bg-gray-300 px-4 py-2">
                   <span class="text-sm font-medium text-heading">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 2
+                    Aquí va la respuesta. :)
                   </span>
                 </div>
               </div>
