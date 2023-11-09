3d3e7a82b24b5b9cc244f3122a3291d7e30c68cc
Daniel martínez <dmartinezh97@gmail.com>
2023-11-10 00:09:20 +0100

    Add verticalnav

---
+++ front/src/@layouts/components/VerticalNav.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <div class="nav-header">
+            <slot name="nav-header"></slot>
+        </div>
+        <slot name="nav-items">
+
+        </slot>
+    </div>
+</template>
