ddd131a935ebfc86f9799c4b146ebb2de2490ffc
Daniel martínez <dmartinezh97@gmail.com>
2023-11-10 00:37:13 +0100

    Add vertical-nav y un container para meter navbar, main, footer, etc..

--- front/src/@layouts/components/VerticalNavLayout.vue
+++ front/src/@layouts/components/VerticalNavLayout.vue
@@ -3,14 +3,24 @@
 import VerticalNav from '@layouts/components/VerticalNav.vue'
 
 export default defineComponent({
-  setup(props, { slots }) {
-    return () => {
+    setup(props, { slots }) {
+        return () => {
 
-        const verticalNav = h(VerticalNav)
+            // 👉 Componente de barra navegación vertical
+            const verticalNav = h(VerticalNav)
 
-        return h('div', { class: 'test vertical' }, [verticalNav])
+            // 👉 Componente de navbar que va dentro de verticalNav
+            const navbar = h(
+                'header',
+                { class: 'layout-navbar' },
+            )
 
-    }
-  },
+            // Retornará el div padre con la clase .test-vertical y los siguientes components:
+            // 👉 El componente VerticalNav contendrá la barra vertical lateral izquierda
+            // Renderizará un div con la clase .layout-content-container y dentro de ella meteremos la barra de navegación superior
+
+            return h('div', { class: 'test-vertical' }, [verticalNav, h('div', { class: 'layout-content-container' }, [navbar])])
+        }
+    },
 })
 </script>
